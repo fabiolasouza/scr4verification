c733adbc316b0992d515f60bcbf550b1caf70926
Fabiola Silva <nkfs@aa6-103.bullx>
2022-11-08 10:44:16 +0000

    added query by forecast date

--- scatt/read_fcScatt_db.R
+++ scatt/read_fcScatt_db.R
@@ -1,7 +1,7 @@
 #Function to read data from sqlite file containing scatterometer and forecast data
-#read_fcScatt_db(start_date = '2022-07-01',
-#		end_date = '2022-07-30',
-#		fcst_model = 'trunk_r17057_update',
+#read_fcScatt_db(start_date = '2022070100',
+#		end_date = '2022070103',
+#		fcst_model = 'WFP_43h22tg3',
 #		scatt = 'HSCAT',
 #		path = '/ec/res4/scratch/nkfs/data/verif_scatterometer/hy-2b/25km',
 #		param = 'S10m',
@@ -10,17 +10,17 @@
 #		lonW=0,
 #		lonE=11,
 #		fcst=seq(0,12,6))
-#TODO: query by dates
+#TODO: query by cycle
 read_fcScatt_db <- function(start_date, end_date, fcst_model, scatt, path, param, latN, latS, lonW, lonE, fcst){
         message("Parameter: ", param)
         fcst_out <- list()
         file_list <- file.path(path, fcst_model, paste('HA_', {{scatt}}, '_', {{param}},'_', {{fcst_model}}, '.sqlite', sep=''))
         for (file in file_list) {
                 message("Reading: ",file )
-		#start_date <- julian(as.Date(paste(start_date)))
-		#end_date <- julian(as.Date(paste(end_date)))
+		start_date <- harpIO::str_datetime_to_unixtime(paste(start_date))
+		end_date <- harpIO::str_datetime_to_unixtime(paste(end_date))
                 con <- DBI::dbConnect(RSQLite::SQLite(), file, flags = RSQLite::SQLITE_RO, synchronous = NULL)
-                df <- DBI::dbGetQuery(con, 'SELECT * FROM SCATT_FCST WHERE lat_scatt<= ? AND lat_scatt>= ? AND lon_scatt>= ? AND lon_scatt<= ?', params<-list(latN, latS, lonW, lonE))
+                df <- DBI::dbGetQuery(con, 'SELECT * FROM SCATT_FCST WHERE lat_scatt<= ? AND lat_scatt>= ? AND lon_scatt>= ? AND lon_scatt<= ? AND fcdate >= ? AND fcdate <= ?', params<-list(latN, latS, lonW, lonE, start_date, end_date))
 		df$fcdate <- harpIO::unix2datetime(df$fcdate)
 		df$validdate <- harpIO::unix2datetime(df$validdate)
 		df <- subset(df, leadtime %in% fcst)
